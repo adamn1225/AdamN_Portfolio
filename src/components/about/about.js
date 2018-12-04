cb968715efdecadbc4c97787ecb946edc9fb030d
Adam <Anoah1225@gmail.com>
2018-12-04 10:52:41 -0500

    Work experience, resume download, social links, profile picture updated

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,23 +10,22 @@ export default class Header extends Component {
             </div>
             <div className="nine columns main-col">
               <h2>About Me</h2>
-              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam
-                voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-                voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-                sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-                Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.
+              <p>My name is Adam Noah. I was born and raised in Lakewood, NJ where I learned judaic and secular studies.
+                At age 18 I moved down to Boca Raton, FL. I have 3 experience working in sales as a logistics consultant
+                and a years experience in marketing for a social media marketing company and a non profit organization.
+                I decided to follow my passion of becoming a full stack web developer and enrolled in Wyncode Acadamy where
+                I learned to not only code, but also the meaning of a growth mindset.
               </p>
               <div className="row">
                 <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
-                    <span>Jonathan Doe</span><br />
-                    <span>1600 Amphitheatre Parkway<br />
-                      Mountain View, CA 94043 US
+                    <span>Adam Noah</span><br />
+                    <span>4161 Coral Tree Circle<br />
+                      Coconut Creek, FL 33073 US
                     </span><br />
-                    <span>(123)456-7890</span><br />
-                    <span>anyone@website.com</span>
+                    <span>954-826-4318</span><br />
+                    <span>anoah1225@gmail.com</span>
                   </p>
                 </div>
                 <div className="columns download">
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -17,7 +17,7 @@ export default class Header extends Component {
             </ul>
             <ul className="copyright">
               <li>© Copyright 2014 CeeVee</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Desin by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
             </ul>
           </div>
           <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
